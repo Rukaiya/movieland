321cdf3ac8b9c5d1f7d389f8b34050c61e76df16
Rukaiya Amin <rukaiyaamin36@gmail.com>
2023-05-28 20:41:35 +0600

    finished

--- src/App.js
+++ src/App.js
@@ -1,8 +1,53 @@
-import React from "react";
+import { useState, useEffect } from "react";
+import './App.css';
+import SearchIcon from './search.svg'
+import MovieCard from "./MovieCard";
+
+const API_URL = 'https://www.omdbapi.com?apikey=e8aaaf2e';
 
 const App = () => {
+  const [movies, setMovies] = useState([]);
+  const [searchTerm, serSearchTerm] = useState([]);
+  const searchMovies = async (title) => {
+    const response = await fetch(`${API_URL}&s=${title}`);
+    const data = await response.json();
+    console.log(data.Search);
+    setMovies(data.Search);
+  }
+  useEffect(() => {
+    searchMovies('Encanto')
+  }, [])
   return (
-    <h1>App</h1>
+    <div className="app">
+      <h1>MovieLand</h1>
+
+      <div className="search">
+        <input
+          placeholder="search for movies"
+          value={searchTerm}
+          onChange={(e) => { serSearchTerm(e.target.value)}}
+        />
+        <img
+          src={SearchIcon}
+          alt="search"
+          onClick={() => {searchMovies(searchTerm)}}
+        />
+      </div>
+      {movies?.length > 0
+        ? (
+          <div className="container">
+            {movies.map((movie) =>
+              <MovieCard movie={movie} />
+            )}
+          </div>
+        ) : (
+          <div className="empty">
+            <h2>No movies found</h2>
+          </div>
+        )
+      }
+
+    </div>
   )
 }
 
